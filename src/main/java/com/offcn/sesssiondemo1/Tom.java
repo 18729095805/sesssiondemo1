c16ae77939300811a0268f798123c8a9c4e1834d
offcn-java <1490383982@qq.com>
2021-03-06 15:31:57 +0800

    add by tom

--- src/main/java/com/offcn/sesssiondemo1/Tom.java
+++ src/main/java/com/offcn/sesssiondemo1/Tom.java
@@ -2,6 +2,6 @@ package com.offcn.sesssiondemo1;
 
 public class Tom {
     public static void main(String[] args) {
-System.out.println("测试tom分支修改文件");
+String d="阿达大大";
     }
 }
--- src/main/java/com/offcn/sesssiondemo1/Tom3.java
+++
@@ -1,4 +0,0 @@
-package com.offcn.sesssiondemo1;
-
-public class Tom3 {
-}
